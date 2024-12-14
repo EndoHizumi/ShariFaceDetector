57dccb1b03ad81c2ee7b9ca1253c49e66ff69804
endohizumi <endohizumi0114@gmail.com>
2024-12-14 18:21:10 +0900

    コンフィグファイルを設定した

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import { Core } from "webtalekit/src/core/";
+import { Core } from "webtalekit-alpha/src/core/";
+import Config from "../engineConfig.json";
 // coreをインスタンス化する
 window.onload = function () {
   const game = new Core();
+  game.setConfig(Config);
   game.start();
 };
